1dddd7a5e707749f7491b3c7803509f16b3dc4b1
Janice Hernandez <hernandez.janice.317@gmail.com>
2025-05-01 00:54:00 +0800

    Update SeatingStrategy.java

--- src/examseatingsystem/SeatingStrategy.java
+++ src/examseatingsystem/SeatingStrategy.java
@@ -1,7 +1,25 @@
 package examseatingsystem;
 
+import java.util.Iterator;
 import java.util.List;
 
 public interface SeatingStrategy {
-    void assignSeats(List<Student> students, List<Seat> seats, Exam exam) throws SeatingException;
+    void assignSeats(List<Student> students, List<Seat> availableSeats, Exam exam) throws SeatingException;
+}
+
+class SequentialSeatingStrategy implements SeatingStrategy {
+    @Override
+    public void assignSeats(List<Student> students, List<Seat> availableSeats, Exam exam) throws SeatingException {
+        if (students.size() > availableSeats.size()) {
+            throw new SeatingException("Not enough seats for all students");
+        }
+
+        Iterator<Student> studentIterator = students.iterator();
+
+        for (Seat seat : availableSeats) {
+            if (studentIterator.hasNext() && !seat.isOccupied()) {
+                seat.assignStudent(studentIterator.next());
+            }
+        }
+    }
 }
