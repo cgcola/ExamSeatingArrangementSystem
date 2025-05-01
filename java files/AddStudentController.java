11165fadccf70669d45607e0d2db3dfc92ad38ab
Sean Booker C. Orioque <seanorioque23@gmail.com>
2025-05-01 14:15:19 +0800

    Create AddStudentController.java

---
+++ java files/AddStudentController.java
@@ -0,0 +1,28 @@
+package com.example.examseatingarangementfinal;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddStudentController implements Initializable {
+
+    @FXML private TextField courseCodeField;
+    @FXML private TextField courseNameField;
+    @FXML private TextField examDateTimeField;
+    @FXML private TextField studentIdField;
+    @FXML private TextField studentNameField;
+    @FXML private TextField sectionField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        courseCodeField.requestFocus();
+    }
+
+
+
+
+
+}
